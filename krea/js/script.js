8a2cd5f8d1814f237a81595b166a92ec2941f9f6
CorvuZ1 <64122587+CorvuZ1@users.noreply.github.com>
2020-11-12 17:33:28 +1000

    Add files via upload

---
+++ krea/js/script.js
@@ -0,0 +1,29 @@
+window.addEventListener("scroll", hashDelete)
+        function hashDelete() {
+            if (window.location.hash) {
+               history.pushState(null,null,"index.html");
+            }   
+        }
+        
+        var servicesBtn = document.querySelector(".services_button");
+        if (window.matchMedia("(max-width:1100px)").matches) {
+            document.querySelector(".plus3").appendChild(servicesBtn);
+        }
+        
+        var navList = document.querySelectorAll("nav ul li a");
+        var mobileMenu = document.querySelector(".mobile-menu")
+        if (window.matchMedia("(max-width:435px)").matches) {
+            for(var a of Array.from(navList)) {
+                a.classList.add("nav-link")
+                mobileMenu.appendChild(a);
+            }
+        }
+        
+        var burger = document.querySelector(".burger");
+        var mobileMenu = document.querySelector(".mobile-menu")
+        burger.addEventListener("click", burgerActivated);
+        
+        function burgerActivated() {
+            mobileMenu.classList.toggle("mobile-menu-close");
+            burger.classList.toggle("burger_active");
+        }
