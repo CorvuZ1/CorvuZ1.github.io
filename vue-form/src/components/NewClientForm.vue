3b9832e3d0bba336e19e812a31ade6fa4caa7b91
CorvuZ1 <superman876@mail.ru>
2020-12-09 16:48:07 +1000

    add vue-form

---
+++ vue-form/src/components/NewClientForm.vue
@@ -0,0 +1,85 @@
+<template>
+  <div class="content-box">
+    <h1 class="title">Регистрация пациента</h1>
+    <div class="img">
+    <form class="form">
+      <div class="wrapper">
+        <FirstStep/>
+        <SecondStep/>
+        <ThirdStep/>
+      </div>
+      <input class="submit" type="submit">
+    </form>
+    </div>
+  </div>
+</template>
+
+<script>
+  import FirstStep from "./first-step/FirstStep"
+  import SecondStep from "./second-step/SecondStep"
+  import ThirdStep from "./third-step/ThirdStep"
+
+  export default {
+      components: {
+        FirstStep,
+        SecondStep,
+        ThirdStep
+      }
+  }
+</script>
+
+<style lang="sass" scoped>
+  $dark-purple: #4e3c7a
+
+  .content-box
+    width: 100%
+    max-width: 1200px
+    border: 2px solid white
+    border-radius: 7px
+    margin: 20px 0
+    min-height: 650px
+    background-color: rgba(0,0,0,0.7)
+    .title
+      margin: 0
+      height: 50px
+      display: flex
+      justify-content: center
+      align-items: center
+      background-color: $dark-purple
+      border-radius: 7px 7px 0 0
+      text-align: center
+
+    .img
+      position: absolute
+      min-height: 600px
+      position: relative
+      background-repeat: no-repeat
+      background-position: left
+      background-size: cover
+      border-radius: 0px 0px 7px 7px
+
+    .wrapper
+      width: 100%
+      min-height: 490px
+      display: flex
+      justify-content: center
+      align-items: center
+      flex-wrap: wrap
+      padding: 30px 0 20px 0
+
+    .submit
+      display: block
+      width: 100%
+      max-width: 400px 
+      height: 50px
+      margin: 9px auto 0 auto
+      z-index: 10
+      background-color: $dark-purple
+      border: none
+      color: white
+      font-family: Montserrat-m
+      font-size: 17px
+      border-radius: 7px 7px 0 0
+      cursor: pointer
+</style>
+
---
+++ vue-form/src/components/first-step/FirstStep.vue
@@ -0,0 +1,129 @@
+<template>
+  <!-- 9 blocks -->
+  <fieldset class="step">
+    <legend>Основная <br> информация</legend>
+
+    <div class="form-item">
+    <label for="first-1">Фамилия</label>
+    <input id="first-1" type="text">
+  </div>
+
+    <div class="form-item">
+    <label for="first-2">Имя</label>
+    <input id="first-2" type="text">
+    </div>
+
+    <div class="form-item">
+    <label for="first-3">Отчество</label>
+    <input id="first-3" type="text">
+    </div>
+
+    <div class="form-item">
+    <label for="first-4">Дата рождения</label>
+    <input id="first-4" type="date">
+    </div>
+
+    <div class="form-item">
+    <label for="first-5">Номер телефона</label>
+    <input id="first-5" type="number">
+    </div>
+
+    <div class="form-item">
+    <label for="first-6">Пол</label>
+    <select id="first-6">
+      <option value="Мужчина">Мужчина</option>
+      <option value="Женщина">Женщина</option>
+    </select>
+    </div>
+
+    <div class="form-item">
+    <label for="group">Группа</label>
+    <select id="group" multiple>
+      <option value="VIP">VIP</option>
+      <option value="Проблемные">Проблемные</option>
+      <option value="ОМС">ОМС</option>
+    </select>
+  </div>
+
+  <div class="form-item">
+    <label for="first-8">Лечащий врач</label>
+    <select id="first-8">
+      <option value="Иванов">Иванов</option>
+      <option value="Захаров">Захаров</option>
+      <option value="Чернышева">Чернышева</option>
+    </select>
+  </div>
+
+  <div class="form-item">
+  <label for="first-9">Не отправлять СМС</label>
+  <input id="first-9" type="checkbox">
+  </div>
+
+  </fieldset>
+</template>
+
+<style lang="sass">
+  .step
+    display: flex
+    justify-content: center
+    flex-wrap: wrap
+    z-index: 5
+    margin-top: 40px auto 0 auto
+    width: 100%
+    
+    input, select
+      margin: 15px 15px
+      border-radius: 50px
+      border: none
+      height: 23px
+      padding: 0 10px
+      font-family: "Montserrat-m"
+      width: 150px
+
+  #group
+    overflow: hidden
+    height: 51px  
+    border-radius: 5px
+
+  input[type='number']
+    -moz-appearance:textfield
+
+  input::-webkit-outer-spin-button,
+  input::-webkit-inner-spin-button 
+    -webkit-appearance: none
+
+  input[type="date"]::-webkit-calendar-picker-indicator
+    display: none
+    -webkit-appearance: none
+
+  legend
+    margin-bottom: 13px
+    font-size: 19px
+    color: white
+    font-family: Montserrat-b
+    text-align: center
+    margin: 20px 0
+    border-radius: 50px
+    border: 2px solid gray
+    padding: 6px 15px
+
+  label
+    color: white
+    font-family: Montserrat-m
+    font-size: 14px
+
+  .form-item
+    display: flex
+    align-items: center
+    flex-direction: column
+
+  fieldset
+    border-top: 2px solid gray
+
+  .step2 
+    margin: 30px 0
+  
+
+</style>
+
+
